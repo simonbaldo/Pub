464e02e35f0b5fa251be9c9313363b1f0fb8299e
Simone Baldo <120671869+simonbaldo@users.noreply.github.com>
2023-02-05 18:32:03 +0100

    Update main.cpp

--- NixieClock/src/main.cpp
+++ NixieClock/src/main.cpp
@@ -10,13 +10,14 @@ Pulsante p1(18, 3); //pin pulsante A4, secondi per attivare il flag longPress
 MyClock cl(999);
 
 MyTimer tm(11000);   //imposto un timer ogni 11 secondi per la visualizzazione
-MyTimer ti(900);     //imposto un timer per la visualizzazione delle impostazioni
+MyTimer ti(900);     //imposto un timer per la visualizzazione delle cifre
 
 boolean longPress=false;
 boolean visCifra=false;
 boolean visualizzaOra=false;
 boolean dispAcceso=true;
 boolean acqOra=true;
+boolean mostraOraSpot=false;
 //boolean initDisplay=true;
 
 uint32_t tpinitD=0;   
@@ -27,10 +28,13 @@ uint8_t oraC1;      //prima cifra ora
 uint8_t oraC2;      //seconda cifra ora
 uint8_t minC1;      //prima cifra minuti
 uint8_t minC2;      //seconda cifra minuti
+uint8_t oraCurr;    //ora corrente
+uint8_t minCurr;    //minuti correnti
 uint8_t oraset;    
 uint8_t minset;
 uint8_t catodoPrec;
 uint8_t cifraDisp;
+uint8_t spegniNotte=0; //valori possibili 0, 1. Se 1 il nixie viene spento dalle 11.00 alle 5:59
 //uint16_t delayInitD=400;
 
 //uint8_t catodi[10]={3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; //array contente le uscite per i catodi per le cifre da 0 a 9
@@ -42,6 +46,7 @@ void testPressedButton();
 void display();
 void displayImpostazioni();
 void displayOra();
+void displayOraSpot(); //visualizza ora alla pressione del tasto se sono in modalità nottuna
 void scomponiOra();
 //void cifreInitDisplay();
 void visualizzaCifra();
@@ -60,16 +65,30 @@ void testPressedButton() {
     timeDiff1=p1.released();
     //timeDiff2=p2.released();
 
+    //se premo il tasto mostro l'ora se sono in spegnimento notturno e nella fascia 23:00 - 5:59
+    if (!longPress && timeDiff1 == 1 and spegniNotte == 1  ) {
+		if  (cl.getOra() >=23 || cl.getOra()<=6) {
+		     mostraOraSpot=true;
+			 spegniNotte=0; //disattivo spegnimento
+			 acqOra=true;
+			 cifraDisp=0;
+			 dispAcceso=true;
+			 return;
+		}	 
+	}
+
     if (timeDiff1 == 2) {
    	   if (!longPress) {
 		  longPress=true;
 		  modImpo=1;
 		  oraset=0;
 		  minset=0;
-		  oraC1=0;
-		  oraC2=0;
-		  minC1=0;
-		  minC2=0;
+		//oraC1=0;
+		//oraC2=0;
+		//minC1=0;
+		//minC2=0;
+		  scomponiOra();
+		  spegniNotte=0;
 		  return;
 	  }
     }    
@@ -77,7 +96,6 @@ void testPressedButton() {
     //if (longPress && timeDiff2 > 0 ) {
 	if (longPress && timeDiff1 == 1 ) {	
        if (modImpo==1) {
-       // cl.addOra();
 	      oraC1++;
 	      if (oraC1 == 3) {
 		     oraC1=0; 
@@ -107,12 +125,20 @@ void testPressedButton() {
 	      }		  
           return;
        }
+       if (modImpo==5) {
+	      spegniNotte++; 
+	      if (spegniNotte == 2) {
+		     spegniNotte=0; 
+	      }		  
+          return;
+       }
+
     }   
 
     //if (longPress && (timeDiff1 >0)) {
 	if (longPress && (timeDiff1 == 2)) {	
           modImpo++;
-          if  (modImpo==5) {
+          if  (modImpo==6) {
               longPress=false;
               modImpo=0;
 			  oraset=(uint8_t)(oraC1*10 + oraC2);
@@ -147,9 +173,17 @@ void dispOra(uint8_t cc) {
      Serial.print("\r");	
      Serial.print(str);
 
-	 if (cc!=99)
+     //spegniNotte = 0 accende il display, mentre spegniNotte=1 spegne il display dalle 23 alle 5:59
+	 if (cc!=99 && spegniNotte == 0 ) 
 		 digitalWrite(catodi[cc], HIGH);
-
+	 else	 
+	 if (cc!=99 && spegniNotte == 1 &&  oraCurr<23 && oraCurr>5)
+		 digitalWrite(catodi[cc], HIGH);
+     else		 
+	 //in modalità impostazioni visualizzo sempre la cifra 
+     if (cc!=99 && modImpo>0)
+	    digitalWrite(catodi[cc], HIGH);
+     	 
 	 catodoPrec=cc;
 }
 
@@ -172,6 +206,9 @@ void displayImpostazioni() {
 			else
 			if (modImpo==4) 
 			   dispOra(minC2);			    
+			else
+			if (modImpo==5) 
+			   dispOra(spegniNotte);			    
 		}	
 		else {
 		      dispOra(99);
@@ -187,11 +224,13 @@ void displayImpostazioni() {
 //scomposizione dell'ora nelle singole cifre
 void scomponiOra() {
 	
-	oraC1=(uint8_t)(cl.getOra() / 10);
-	oraC2=(uint8_t)(cl.getOra() % 10);
+	oraCurr=(uint8_t)cl.getOra();
+	oraC1=(uint8_t)(oraCurr / 10);
+	oraC2=(uint8_t)(oraCurr % 10);
 	
-	minC1=(uint8_t)(cl.getMin() / 10);
-	minC2=(uint8_t)(cl.getMin() % 10);	
+	minCurr=(uint8_t)cl.getMin();
+	minC1=(uint8_t)(minCurr / 10);
+	minC2=(uint8_t)(minCurr % 10);	
 }
 
 /*
@@ -212,7 +251,54 @@ void cifreInitDisplay() {
 }
 */
 //display delle cifre dell'ora
+void displayOraSpot() {
+	
+	if (acqOra) {
+		scomponiOra();
+		acqOra=false;
+	}
+	   		   
+	ti.run();
+
+	//visCifra viene attivata dalla cback di ti.run();	
+	if (visCifra) {
+		cifraDisp++;				   
+		
+		if (dispAcceso) {
+			if (cifraDisp==1) 
+				dispOra(oraC1);
+			else
+			if (cifraDisp==3) 
+				dispOra(oraC2);			    
+			else
+			if (cifraDisp==5) 
+				dispOra(minC1);			    
+			else
+			if (cifraDisp==7) 
+				dispOra(minC2);			    			   
+		}
+		else {
+			dispOra(99);
+		} 
+		
+		visCifra=false;
+		dispAcceso=!dispAcceso;
+		if  (cifraDisp==8) {
+			mostraOraSpot=false;
+			spegniNotte=1;
+		}
+	}	   
+	
+}
+
+//display delle cifre dell'ora
 void displayOra() {
+
+    //visualizzazione ora a seguito della pressione del tasto in modlaità spegnimento notturno
+    if (mostraOraSpot) {
+		displayOraSpot();
+		return;
+	}
 	
    //visualizzaOra viene attivata dalla cback di tm.run()
    if  (visualizzaOra) {
@@ -285,7 +371,12 @@ void visualizzaCifra() {
 
 //callback di visualizzazione dell'ora
 void visOra() {
-	
+
+  //se sono in visualizzazione dell'orario in fase notturna non aggiorno i flag ed i contatori delle cifre
+  //poichè ci pensa la function di verifica del pulsante a valorizzarli
+  if (mostraOraSpot)
+	return;
+  	
   visualizzaOra=true; 
   dispAcceso=true;  
   acqOra=true;
@@ -295,7 +386,7 @@ void visOra() {
 }
 
 
-//dummy function per callback del clocl
+//dummy function per callback del clock
 void dummyFunc() {
 
 }
@@ -313,6 +404,8 @@ void setup() {
   //inizializzazione dell'orologio
   cl.init();
   //definizione della callback - Verrà chiamata la funzione ad ogni intervallo prestabilito
+  //chiamo una dummy function perchè per il momento non serve. E' necessario definirla anche se vuota
+  //per non causare comportamenti aleatori
   cl.cback(dummyFunc);
   
   //imposto timer per visualizzazione delle cifre
@@ -325,6 +418,7 @@ void setup() {
   //impostazione callback a scatto del timer  per visualizzazione dell'ora
   tm.cback(visOra);  
 
+  delay(2000);
   //impostazione pinout delle uscite dei catodi e spegimento del nixie
   for (int i=0;i<10;i++) {
 	  pinMode(catodi[i], OUTPUT);
@@ -340,15 +434,16 @@ void setup() {
 //main loop
 void loop() {
 
-  //avvio timer per visualizzazione cifre orologio
-    tm.run();
+//avvio timer per visualizzazione cifre orologio
+  tm.run();
 	
-  //avvio orologio
-    cl.run();
-
-  //verifica pressione dei pulsanti per impostazione ora
-    testPressedButton();	
+//avvio orologio
+  cl.run();
 	
-  //mostro ora oppure impostazioni 
-    display();
+//verifica pressione dei pulsanti per impostazione ora
+  testPressedButton();	
+	
+//mostro ora oppure impostazioni 
+  display();
 }
+
